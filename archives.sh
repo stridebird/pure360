32d677ed8537e83733c1d4696af3d710da9fb2a4
pete <pete@stridebird.com>
2013-05-24 07:12:04 +0100

    regenerated archives

--- archives.sh
+++ archives.sh
@@ -1,2 +1,2 @@
-zip pure360demo.zip css/ csvfiles/ index.php drawgraph.php library.php template.php README.md
+zip -r pure360demo.zip css/ csvfiles/ index.php drawgraph.php library.php template.php README.md
 tar cvfz  pure360demo.tar.gz css/ csvfiles/ index.php drawgraph.php library.php template.php README.md
